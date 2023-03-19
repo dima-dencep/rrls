dd21dd9d20b3f1bcbabd74583ddead462a269a63
dima_dencep <dima_dencep@turbomc.tk>
2023-03-19 19:39:20 +0700

    Fix build

--- common/src/main/java/com/github/dimadencep/mods/rrls/mixins/MinecraftClientMixin.java
+++ common/src/main/java/com/github/dimadencep/mods/rrls/mixins/MinecraftClientMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,7 +32,7 @@ public abstract class MinecraftClientMixin {
         if (!MainMod.config.resetResources) {
             this.reloadResources(true).thenRun(() -> {
                 ToastManager toastManager = this.getToastManager();
-                SystemToast.show(toastManager, SystemToast.Type.PACK_LOAD_FAILURE, Text.translatable("resourcePack.load_fail"), resourceName == null ? Text.translatable("gui.all") : resourceName);
+                SystemToast.show(toastManager, SystemToast.Type.PACK_LOAD_FAILURE, new TranslatableText("resourcePack.load_fail"), resourceName == null ? new TranslatableText("gui.all") : resourceName);
             });
 
             ci.cancel();
@@ -49,7 +50,7 @@ public abstract class MinecraftClientMixin {
         if (MainMod.reloadHandler.getReload() != null) {
             cir.setReturnValue(CompletableFuture.runAsync(() -> {
                 ToastManager toastManager = this.getToastManager();
-                SystemToast.show(toastManager, SystemToast.Type.PACK_LOAD_FAILURE, Text.translatable("resourcePack.load_fail"), Text.translatable("rrls.alreadyReloading"));
+                SystemToast.show(toastManager, SystemToast.Type.PACK_LOAD_FAILURE, new TranslatableText("resourcePack.load_fail"), new TranslatableText("rrls.alreadyReloading"));
             }));
         }
     }
