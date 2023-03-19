4f38898f5f2ad04f2427a820e01dffa73454c72e
dima_dencep <dima_dencep@turbomc.tk>
2023-03-19 17:43:56 +0700

    Fix #1

--- common/src/main/java/com/github/dimadencep/mods/rrls/mixins/MinecraftClientMixin.java
+++ common/src/main/java/com/github/dimadencep/mods/rrls/mixins/MinecraftClientMixin.java
@@ -2,54 +2,41 @@ package com.github.dimadencep.mods.rrls.mixins;
 
 import com.github.dimadencep.mods.rrls.MainMod;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.resource.ResourceReloadLogger;
-import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
-    @Shadow @Final private static Logger LOGGER;
-
+    @Shadow protected abstract void showResourceReloadFailureToast(@Nullable Text description);
     @Shadow protected abstract CompletableFuture<Void> reloadResources(boolean force);
 
-    @Shadow @Final public GameOptions options;
+    @Inject(
+            method = "onResourceReloadFailure",
+            at = @At(
+                    value = "HEAD"
+            ),
+            cancellable = true
+    )
+    public void onResourceReloadFailure(Throwable exception, Text resourceName, CallbackInfo ci) {
+        if (!MainMod.config.resetResources) {
+            this.reloadResources(true).thenRun(() -> this.showResourceReloadFailureToast(resourceName));
 
-    @Shadow @Final private ResourceReloadLogger resourceReloadLogger;
-
-    @Shadow @Final private ResourcePackManager resourcePackManager;
-
-    @Shadow protected abstract void showResourceReloadFailureToast(@Nullable Text description);
-
-    /**
-     * @author dima_dencep
-     * @reason fix on failed load resources
-     */
-    @Overwrite
-    public void onResourceReloadFailure(Throwable exception, @Nullable Text resourceName) {
-        LOGGER.info("Caught error loading resourcepacks, removing all selected resourcepacks", exception);
-
-        if (MainMod.config.resetResources) {
-            this.resourceReloadLogger.recover(exception);
-            this.resourcePackManager.setEnabledProfiles(Collections.emptyList());
-            this.options.resourcePacks.clear();
-            this.options.incompatibleResourcePacks.clear();
-            this.options.write();
+            ci.cancel();
         }
+    }
 
-        this.reloadResources(true).thenRun(() -> this.showResourceReloadFailureToast(resourceName == null ? Text.translatable("gui.all") : resourceName));
+    @ModifyArg(method = "showResourceReloadFailureToast", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/toast/SystemToast;show(Lnet/minecraft/client/toast/ToastManager;Lnet/minecraft/client/toast/SystemToast$Type;Lnet/minecraft/text/Text;Lnet/minecraft/text/Text;)V", remap = false), index = 3)
+    public Text createDisplay(Text resourceName) {
+        return resourceName == null ? Text.translatable("gui.all") : resourceName;
     }
 
     @Inject(
