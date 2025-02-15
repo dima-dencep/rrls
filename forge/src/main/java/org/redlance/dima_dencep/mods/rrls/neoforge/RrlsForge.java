64e4baea74c6cafd76d57593b87747967a743721
dima_dencep <dima_dencep@redlance.org>
2025-02-15 14:59:48 +0700

    Fix #132

--- forge/src/main/java/org/redlance/dima_dencep/mods/rrls/neoforge/RrlsForge.java
+++ forge/src/main/java/org/redlance/dima_dencep/mods/rrls/neoforge/RrlsForge.java
@@ -23,7 +23,7 @@ import org.redlance.dima_dencep.mods.rrls.screens.ConfigurationSectionScreenProx
 @Mod(value = Rrls.MOD_ID, dist = Dist.CLIENT)
 public class RrlsForge extends Rrls {
     public RrlsForge(ModContainer container) {
-        container.registerConfig(ModConfig.Type.CLIENT, RrlsConfig.CONFIG_SPEC_PAIR.getValue(), "rrls.toml");
+        container.registerConfig(ModConfig.Type.STARTUP, RrlsConfig.CONFIG_SPEC_PAIR.getValue(), "rrls.toml");
         container.registerExtensionPoint(IConfigScreenFactory.class, (modContainer, screen) ->
                 new ConfigurationScreen(modContainer, screen, ConfigurationSectionScreenProxy::new)
         );
