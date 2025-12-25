3b1ddfe40a54a753f84e06c535f70caf9541973d
dima_dencep <dima_dencep@redlance.org>
2025-12-26 01:03:43 +0700

    sharedstate

--- common/src/main/java/org/redlance/dima_dencep/mods/rrls/mixins/workaround/ReloadableResourceManagerMixin.java
+++ common/src/main/java/org/redlance/dima_dencep/mods/rrls/mixins/workaround/ReloadableResourceManagerMixin.java
@@ -30,6 +30,7 @@ import net.minecraft.util.Util;
 import org.redlance.dima_dencep.mods.rrls.Rrls;
 import org.redlance.dima_dencep.mods.rrls.RrlsConfig;
 import org.redlance.dima_dencep.mods.rrls.utils.OverlayHelper;
+import org.redlance.dima_dencep.mods.rrls.utils.WaitingSharedState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -130,7 +131,7 @@ public class ReloadableResourceManagerMixin {
 
             Rrls.LOGGER.info("Quick reload listener '{}'", listener.getName());
 
-            PreparableReloadListener.SharedState sharedState = new PreparableReloadListener.SharedState((ReloadableResourceManager) (Object) this);
+            PreparableReloadListener.SharedState sharedState = new WaitingSharedState((ReloadableResourceManager) (Object) this);
             listener.prepareSharedState(sharedState);
 
             listener.reload(sharedState, Util.backgroundExecutor(), CompletableFuture::completedFuture, gameExecutor)
---
+++ common/src/main/java/org/redlance/dima_dencep/mods/rrls/utils/WaitingSharedState.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 - 2025 dima_dencep.
+ *
+ * Licensed under the Open Software License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *     https://spdx.org/licenses/OSL-3.0.txt
+ */
+
+package org.redlance.dima_dencep.mods.rrls.utils;
+
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.jspecify.annotations.NonNull;
+
+import java.util.concurrent.CompletableFuture;
+
+@SuppressWarnings("unchecked")
+public class WaitingSharedState extends PreparableReloadListener.SharedState {
+    public WaitingSharedState(ResourceManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public <T> T get(PreparableReloadListener.@NonNull StateKey<T> key) {
+        try {
+            return super.get(key);
+        } catch (Throwable th) {
+            try {
+                return (T) CompletableFuture.failedFuture(th);
+            } catch (ClassCastException e) {
+                throw th;
+            }
+        }
+    }
+}
+
