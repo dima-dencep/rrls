39436d46480ed38d647b41920a1bf1324455fcf3
dima_dencep <dima_dencep@redlance.org>
2026-02-07 16:01:31 +0700

    Update header

--- common/src/main/java/org/redlance/dima_dencep/mods/rrls/mixins/workaround/GuiGraphicsMixin.java
+++ common/src/main/java/org/redlance/dima_dencep/mods/rrls/mixins/workaround/GuiGraphicsMixin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 - 2025 dima_dencep.
+ * Copyright 2023 - 2026 dima_dencep.
  *
  * Licensed under the Open Software License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
